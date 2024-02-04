9f756e6a09af5c7abdc164dc9c318a66f205f60f
elifnebioglu <elifnebioglu@hotmail.com>
2024-02-04 19:43:09 +0300

    Initial implementation of frontend (Background)

---
+++ frontEnd/src/App.js
@@ -0,0 +1,14 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import Background from "./components/Background";
+
+
+const App = () => {
+
+  return (
+    <Background/>
+  );
+};
+
+export default  App;
